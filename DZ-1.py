84d3766014c271ed8f42c9fb2113b8f9f2cd6ecf
romiras-coder <rom_iras@mail.ru>
2020-06-05 02:39:30 +0300

    Initial commit

---
+++ DZ-1.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+link = 'https://api.github.com/users/'
+user = 'romiras-coder'
+repos = 'repos'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'Accept': 'application/vnd.github.v3+json'}
+response = requests.get(f'{link}{user}/{repos}')
+data = response.json()
+a = json.dumps(data, ensure_ascii=False, indent=4)
+with open('DZ-1.json', 'wb') as f:
+    f.write(a.encode('utf-8'))
